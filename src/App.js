5fe2e71aa6408fdb0ddea0103c1bf20e1966a319
willianoliva <contato@melhor-ia.com>
2025-02-14 21:55:17 -0300

    topfold updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Topfold from './assets/views/Topfold';
+import Header from './assets/views/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Topfold />
     </div>
   );
--- src/assets/views/Topfold.js
+++ src/assets/views/Topfold.js
@@ -3,43 +3,16 @@ import React from "react"
 import "./Topfold.css"
 import VideoModal from '../components/VideoModal';
 
-const openLinkInNewTab = (link) => {
-  window.open(link, "_blank", "noopener,noreferrer");
-};
-
 const Topfold = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
   const [isVideoOpen, setIsVideoOpen] = useState(false)
 
   return (
     <div className="topfold">
-      <header>
-        <div className="logo">Melhor-IA</div>
-        <nav className="desktop-nav">
-          <a href="#">Soluções</a>
-          <a href="#">Casos de Sucesso</a>
-          <a href="#">Contato</a>
-          <button className="login-btn"
-          onClick={()=> openLinkInNewTab("http://google.com")}>Login
-          </button>
-        </nav>
-        <button className="menu-toggle" onClick={() => setIsMenuOpen(!isMenuOpen)}>
-          {isMenuOpen ? "X" : "☰"}
-        </button>
-      </header>
-
-      {isMenuOpen && (
-        <nav className="mobile-nav">
-          <a href="#">Soluções</a>
-          <a href="#">Casos de Sucesso</a>
-          <a href="#">Contato</a>
-        </nav>
-      )}
 
       <main>
         <div className="content">
           <h1>
-            Conheça a AGATA.
+            <reg>Conheça a AGATA.</reg>
             <span>seu Agente de Atendimento Automático</span>
           </h1>
           <p>
@@ -69,7 +42,7 @@ const Topfold = () => {
         <div className="visual">
           <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
             <path
-              fill="#d0c1a9"
+              fill="var(--kraft-accent)"
               d="M44.7,-76.4C58.8,-69.2,71.8,-59.1,79.6,-45.8C87.4,-32.6,90,-16.3,88.5,-0.9C87,14.6,81.4,29.2,73.4,42.1C65.4,55,55,66.3,42.1,74.4C29.2,82.5,14.6,87.4,-0.7,88.6C-16.1,89.8,-32.2,87.3,-45.6,79.3C-58.9,71.3,-69.5,57.9,-76.9,43.3C-84.3,28.7,-88.5,14.3,-88.2,0.2C-87.9,-14,-83,-28,-75.3,-40.4C-67.6,-52.8,-57,-63.6,-44.3,-71.5C-31.6,-79.5,-15.8,-84.6,-0.2,-84.3C15.4,-84,30.7,-78.3,44.7,-76.4Z"
               transform="translate(100 100)"
             />
